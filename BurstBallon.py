75a7dae94733d2c8f01bb903a1a97323210da8ba
baoooliang <73327355+baoooliang@users.noreply.github.com>
2020-11-06 15:27:09 -0800

    Create BurstBallon.py

---
+++ BurstBallon.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        @lru_cache(None)
+        def dp(left, right):
+            if left + 1 == right: return 0
+            return max(nums[left] * nums[i] * nums[right] + dp(left, i) + dp(i, right) for i in range(left+1, right))
+        return dp(0, len(nums)-1)
